add12c1605dfca38f747494327067134a0d5a196
lowlyphe <jones.kyle2893@gmail.com>
2022-06-24 21:28:53 -0500

    cleaned up code

--- index.js
+++ index.js
@@ -7,75 +7,50 @@ let goLight = document.getElementById("goLight");
 
 //main listeners for bulb colors
 stopButton.addEventListener("click", () => {
-  if (stopLight.style.backgroundColor !== "red") {
-    stopLight.style.backgroundColor = "red";
-  } else {
-    stopLight.style.backgroundColor = "#111";
-  }
+  stopLight.classList.toggle("stop");
 });
 
 slowButton.addEventListener("click", () => {
-  if (slowLight.style.backgroundColor !== "yellow") {
-    slowLight.style.backgroundColor = "yellow";
-  } else {
-    slowLight.style.backgroundColor = "#111";
-  }
+  slowLight.classList.toggle("slow");
 });
 
 goButton.addEventListener("click", () => {
-  if (goLight.style.backgroundColor !== "green") {
-    goLight.style.backgroundColor = "green";
-  } else {
-    goLight.style.backgroundColor = "#111";
-  }
+  goLight.classList.toggle("go");
 });
 
 //logs mouse state per button
-stopButton.addEventListener("mouseleave", () => {
-  console.log("Left stop button");
+[stopButton, slowButton, goButton].forEach((button) => {
+  button.addEventListener("mouseenter", () => {
+    console.log(`Entered ${button.innerText} button`);
+  });
+  button.addEventListener("mouseleave", () => {
+    console.log(`Left ${button.innerText} button`);
+  });
 });
 
-stopButton.addEventListener("mouseenter", () => {
-  console.log("Entered stop button");
-});
-
-slowButton.addEventListener("mouseleave", () => {
-  console.log("Left slow button");
-});
-
-slowButton.addEventListener("mouseenter", () => {
-  console.log("Entered slow button");
-});
-
-goButton.addEventListener("mouseleave", () => {
-  console.log("Left go button");
-});
-
-goButton.addEventListener("mouseenter", () => {
-  console.log("Entered go button");
-});
-
-//logs bulb state
-stopButton.addEventListener("click", () => {
-  if (stopLight.style.backgroundColor !== "red") {
-    console.log("stop bulb off");
-  } else {
-    console.log("stop bulb on");
-  }
-});
-
-slowButton.addEventListener("click", () => {
-  if (slowLight.style.backgroundColor !== "yellow") {
-    console.log("slow bulb off");
-  } else {
-    console.log("slow bulb on");
-  }
-});
-
-goButton.addEventListener("click", () => {
-  if (goLight.style.backgroundColor !== "green") {
-    console.log("go bulb off");
-  } else {
-    console.log("go bulb on");
-  }
+[stopButton, slowButton, goButton].forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.stopPropagation();
+    if (goLight.classList.value === "bulb" && button.id === "goButton") {
+      console.log(`${button.innerText} off`);
+    } else if (goLight.classList.value !== "bulb" && button.id === "goButton") {
+      console.log(`${button.innerText} on`);
+    }
+    if (slowLight.classList.value === "bulb" && button.id === "slowButton") {
+      console.log(`${button.innerText} off`);
+    } else if (
+      slowLight.classList.value !== "bulb" &&
+      button.id === "slowButton"
+    ) {
+      console.log(`${button.innerText} on`);
+    }
+    if (stopLight.classList.value === "bulb" && button.id === "stopButton") {
+      console.log(`${button.innerText} off`);
+    } else if (
+      stopLight.classList.value !== "bulb" &&
+      button.id === "stopButton"
+    ) {
+      console.log(`${button.innerText} on`);
+    }
+  });
 });
